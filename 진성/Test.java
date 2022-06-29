393c9a1278a080069794e10d3c06f8c3cb5fbf7d
Administrator <Administrator@SC-202106161522>
2022-06-29 12:24:03 +0900

    내가 최종 승리자임

--- 진성/Test.java
+++ 진성/Test.java
@@ -1,14 +1,15 @@
 
 public class Test {
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("완성시킴"); // 컷!
 		System.out.println("싫다 계속할끄다");
 		//맘에 들때까지 
+		//내가 이김 깝 ㄴㄴ
+		//알겠니?
 
 
-=======
-		System.out.println("내가 충돌시킴");
->>>>>>> branch 'main' of https://github.com/seokgoooo/ProjectLotto.git
+
+		
 	}
 }
