48516cb6c47da78f84ee20c9f588486b6bec3f77
Administrator <Administrator@SC-202106161522>
2022-06-29 12:21:42 +0900

    이제 그만해요..

--- 진성/Test.java
+++ 진성/Test.java
@@ -3,23 +3,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("완성시킴"); // 컷!
 
-		System.out.println("와우"); // 다음!
-		System.out.println("뭐냐고 이거");
-
-		System.out.println("동완햄 뭐함");
-		System.out.println("아아아아");
-		System.out.println("아아아아");
-
-		System.out.println("배고파");
-
-		System.out.println("진성이바보");
-		System.out.println("진성이바보");
-		System.out.println("진성이바보");
-		System.out.println("진성이바보");
-		System.out.println("진성이바보");
-		//내가 다이길거임
-		//내가 짱임 
-		//건들지마셈
+		//맘에안든다
 
 
 	}
