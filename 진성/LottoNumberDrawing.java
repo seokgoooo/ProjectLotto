9388797c22aabd4539cd6b3a4b5571f714d6598b
Administrator <Administrator@SC-202106161522>
2022-07-01 14:04:33 +0900

    gg

--- 진성/LottoNumberDrawing.java
+++ 진성/LottoNumberDrawing.java
@@ -11,6 +11,7 @@ public class LottoNumberDrawing {
 	private Set<Integer> set;// Set 배열 필드
 	private Integer bonus; // 보너스 필드
 	private Random random = new Random(); // 랜덤객체 필드
+//	private Map<String, Set<Integer>> map;
 	
 	public LottoNumberDrawing() { // 생성자 : 필드를 재설정할수있음.
 		this.set = new HashSet<>();
@@ -42,25 +43,18 @@ public class LottoNumberDrawing {
 		this.bonus = bonus;
 	}
 
-	public Set<Integer> LottoRandom () {
+	public Set<Integer> LottoRandom () { // 1등  메소드
 		set = new HashSet<>();
+		bonus = (random.nextInt(45) + 1);
 		while (set.size() < 6) { // 세트사이즈만큼
-			set.add(random.nextInt(45) + 1);// 세트에 랜덤 추가 // 이제 객체생성해서 출력하면 바로 첫번째 로또번호 추가.
-
+			set.add(random.nextInt(45) + 1);// 세트에 랜덤 추가 // 이제 객체생성해서 출력하면 바로 첫번째 로또번호 추가.		
 		}
 
-//		while (true) {
-//			bonus = (random.nextInt(45) + 1);
-//			if (!set.contains(bonus)) {
-//				break;
-//			}
-//		}
 		return set;
 		
 	}
 	
-	public Integer bonusNumber(Set<Integer> set) {
-		int bonus = 0;
+	public Integer bonusNumber(Set<Integer> set) { // 보너스넘버 메소드 // 역대번호 메소드에 보너스 번호넣기위해
 		while (true) {
 			bonus = (random.nextInt(45) + 1);
 			if (!set.contains(bonus)) {
@@ -69,22 +63,33 @@ public class LottoNumberDrawing {
 		}
 		return bonus;
 	}
+	public void printLottoRandom(Set<Integer> set) {
+		System.out.println("당첨번호" + set + "보너스 번호" + bonus);
+	}
 	
-	public void LottoRound() {
-		Map<String, Set<Integer>> map = new HashMap<>(); // lotto 번호를 담을 map 배열생성. 스트링으로바꾸면된다? // 링크드해쉬맵이순서대로 -> 해쉬맵으로 key는 스트링타입  value는 LottoNumberDrawing타입
+	public Map<String, Set<Integer>> LottoRound() { // 역대번호 메소드
+		Map<String, Set<Integer>>map = new HashMap<>();
 		int[] bonus = new int[100];
 		// Map배열<왼쪽 String 오른쪽 ??) //Set 배열 담기.
 		for(int i = 0; i < 100; i++) { // 반복문 키도 됨.. value값만.제대로..
-//			LottoNumberDrawing lnd = new LottoNumberDrawing();// 객체생성 
-			Set<Integer> set = LottoRandom();
+			Set<Integer> set = LottoRandom(); // 반복될떄마다 새로운 1등당첨메소드 생성.
 			bonus[i] = bonusNumber(set);
 			map.put(String.valueOf(i), set); // 아이가 추가될떄마다 set도 새로운 랜덤번호가 추가되야함.i,
 		}
 		
-		for(int i = 0;i < map.size();i++) { // 출력 맵사이즈만큼 출력 반복문 100 개겟죠? //반복 현재됨.
-			System.out.println((i + 1) + "회차" + map.get(String.valueOf(i)) + "보너스 번호" + bonus[i]); // i는 o이니 +1시키고 + 회차 그리고  map.get.i키값을 불러와라 불러오면 value도따라온다.
+//		for(int i = 0;i < map.size();i++) { // 출력 맵사이즈만큼 출력 반복문 100 개겟죠? //반복 현재됨.
+//			System.out.println((i + 1) + "회차" + map.get(String.valueOf(i)) + "보너스 번호" + bonus[i]); // i는 o이니 +1시키고 + 회차 그리고  map.get.i키값을 불러와라 불러오면 value도따라온다.
+//		}
+		
+		return map;
+		
+	}
+	
+	public void printMap(Map<String, Set<Integer>> map) { // 역대번호 출력 메소드?
+		
+		for(int i = 0; i < map.size(); i++) {
+			System.out.println((i + 1) + "회차" + map.get(String.valueOf(i)) + "보너스번호" + bonusNumber(set));
 		}
-		return;
 	}
 
 
@@ -96,9 +101,9 @@ public class LottoNumberDrawing {
 
 		
 		LottoNumberDrawing ld = new LottoNumberDrawing();
-		ld.LottoRandom();
-		ld.LottoRound();
-//		System.out.println(ld);
+
+		ld.printLottoRandom(ld.LottoRandom());// 1등메소드출력
+		ld.printMap(ld.LottoRound());
 		
 		
 
