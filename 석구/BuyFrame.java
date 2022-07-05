0adbfd2d4109476155a2206fcc0c93a77c294320
seokgoooo <coaffc@gmail.com>
2022-07-05 16:44:13 +0900

    진성이의 추가수정본
    억까방지

--- 석구/BuyFrame.java
+++ 석구/BuyFrame.java
@@ -467,6 +467,11 @@ public class BuyFrame extends JFrame implements ActionListener {
 		rightBuyBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				if (lottoList.size() > 0 && lottoList.size() <= 6) {
+					JOptionPane.showMessageDialog(BuyFrame.this, "구매완료! 당첨확인 바랍니다");
+				} else {
+					JOptionPane.showMessageDialog(BuyFrame.this, "로또를 구매하세요!");
+				}
 				consumer.setLottoList(lottoList);
 				lottoList.removeAll(lottoList);
 //				----------------------------------------------------------------------------------
